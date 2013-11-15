0668d41693a83ba2f8244ff29087356c1392658c
Yan Wang <grapeot@gmail.com>
2013-11-15 14:28:13 -0500

    add some vim plug in

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 # echo Please install vim and configure sudo manually
 # su 
 
@@ -27,16 +28,23 @@
 # cat 4576917/vimrc | sed 's/consolas:h16/Inconsolata\\ 14/' > ~/.vimrc
 # rm -rf 4576917
 # mkdir ~/.vim
-# cd ~/.vim
+pushd ~/.vim
 # mkdir colors
 # cd colors
 # wget http://files.werx.dk/wombat.vim
-
 # mv wombat.vim wombat2.vim
 # cat wombat2.vim | dos2unix > wombat.vim
 # rm wombat2.vim
 # cd ..
-# cd ~
+wget "http://www.vim.org/scripts/download_script.php\?src_id\=17123" -O nerdtree.zip
+unzip nerdtree.zip
+rm nerdtree.zip
+wget "http://downloads.sourceforge.net/project/vim-latex/snapshots/vim-latex-1.8.23-20130116.788-git2ef9956.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fvim-latex%2Ffiles%2Fsnapshots%2F&ts=1384542687&use_mirror=softlayer-dal" -O latex.tar.gz
+tar -xzvf latex.tar.gz
+mv vim-latex*/* .
+rm -r vim-latex*
+rm latex.tar.gz
+popd
 
 # echo git configuration
 # git config --global user.name "Yan Wang"
@@ -46,7 +54,7 @@
 # sudo bash -c "cat /etc/ssh/sshd_config | sed 's/Port 22/Port 30/' | tee /etc/ssh/sshd_config"
 
 # echo Installing application software
-sudo apt-get install tmux python rsync zip unzip unrar xfce4 xfce4-power-manager xfce4-screenshooter evince pulseaudio cups cups-client ristretto gnome-screensaver scim scim-pinyin ttf-wqy-microhei ttf-wqy-zenhei fonts-inconsolata
+sudo apt-get install tmux python rsync zip unzip unrar xfce4 xfce4-power-manager xfce4-screenshooter xfce4-terminal xfce4-systemload-plugin gvim-gtk evince pulseaudio cups cups-client ristretto gnome-screensaver scim scim-pinyin ttf-wqy-microhei ttf-wqy-zenhei fonts-inconsolata
 # wget http://font.ubuntu.com/download/ubuntu-font-family-0.80.zip
 # unzip ubuntu-font-family-0.80.zip
 # mkdir ~/.fonts
@@ -56,7 +64,8 @@ sudo apt-get install tmux python rsync zip unzip unrar xfce4 xfce4-power-manager
 # cp .tmux.conf ~
 
 # Optional software, uncomment
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
+# sudo apt-get install -f
+# rm google-chrome-stable_current_amd64.deb
 # sudo apt-get install latexmk latex-beamer
