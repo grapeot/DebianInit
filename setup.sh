3ba85e78a886367bfb2c31946052ff0c315eb58a
Yan Wang <grapeot@gmail.com>
2013-11-15 13:09:46 -0500

    add git, ssh and vim configuration

--- setup.sh
+++ setup.sh
@@ -7,8 +7,9 @@
 # sudo apt-get update
 
 # echo Configure development environment
-# sudo apt-get install zsh git tig build-essential wget curl
-# echo Shell environment...
+# sudo apt-get install zsh git tig build-essential wget curl rsync 
+
+# echo shell environment...
 # chsh -s $(which zsh)
 # wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 # cd ~
@@ -21,16 +22,26 @@
 # echo "alias gma='git commit -a -m'" >> ~/.zshrc
 # echo source ~/z/z.sh >> ~/.zshrc
 # 
-echo vim configuration...
+# echo vim configuration...
 # git clone https://gist.github.com/4576917.git
-cat 4576917/vimrc | sed 's/consolas:h16/Inconsolata\ 14/' > ~/.vimrc
-mkdir ~/.vim
-cd ~/.vim
+# cat 4576917/vimrc | sed 's/consolas:h16/Inconsolata\\ 14/' > ~/.vimrc
+# rm -rf 4576917
+# mkdir ~/.vim
+# cd ~/.vim
+# mkdir colors
+# cd colors
+# wget http://files.werx.dk/wombat.vim
+# mv wombat.vim wombat2.vim
+# cat wombat2.vim | dos2unix > wombat.vim
+# rm wombat2.vim
+# cd ..
+# cd ~
 
-cd ~
+# echo git configuration
+# git config --global user.name "Yan Wang"
+# git config --global user.email grapeot@gmail.com
 
-echo git configuration
-git config --global user.name "Yan Wang"
-git config --global user.email grapeot@gmail.com
+echo "ssh configuration (won't be effected until next restart)"
+sudo bash -c "cat /etc/ssh/sshd_config | sed 's/Port 22/Port 30/' | tee /etc/ssh/sshd_config"
 
 # echo Installing application software
