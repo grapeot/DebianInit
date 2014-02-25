9d9f35d113817c5bacb829719bfb7c5f1091e1f1
flyfj <fj333xx@gmail.com>
2014-02-24 19:01:20 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ sudo apt-get update
 
 # Configure development environment
 sudo apt-get install -y -q vim zsh git wget dos2unix python python-setuptools
+# sudo apt-get install libopencv-dev
 # shell environment...
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 pushd ~
