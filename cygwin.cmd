882484b0ab2edad6665756b1cf09ab0189986055
Yan Wang <grapeot@gmail.com>
2014-03-28 09:08:53 -0700

    change cygwin version to x86 and also add a pause

--- cygwin.cmd
+++ cygwin.cmd
@@ -1,3 +1,4 @@
-.\setup-x86_64.exe -P vim -P openssh -P rsync -P gcc-g++ -P make -P wget -P curl -P dos2unix -P git -P tig -P zsh -s http://mirror.cc.columbia.edu/pub/software/cygwin/
+.\setup-x86.exe -P vim -P openssh -P rsync -P gcc-g++ -P make -P wget -P curl -P dos2unix -P git -P tig -P zsh -s http://mirror.cc.columbia.edu/pub/software/cygwin/
+pause
 cmd /c "set PATH=%PATH%;c:\cygwin64\bin; && zsh -i -c 'git clone https://github.com/grapeot/DebianInit.git; cd DebianInit; ./cygwin.sh'"
 echo Don't forget to add the cygwin bin path to the system PATH.
