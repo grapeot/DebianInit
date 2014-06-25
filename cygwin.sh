e761042c17515e24f1267d9b9bc9e5c4b7e5c20b
Yan Wang <grapeot@gmail.com>
2014-06-25 15:16:33 -0400

    update the cygwin version;

--- cygwin.sh
+++ cygwin.sh
@@ -14,29 +14,17 @@ wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master
 pushd ~
 git clone https://github.com/rupa/z
 popd
+# .dotfiles
+pushd ~
+git clone https://github.com/grapeot/.dotfiles
 # .zshrc
-git clone https://gist.github.com/8777085.git
-mv 8777085/.zshrc ~
-rm -rf 8777085
-# vim configuration
-git clone https://gist.github.com/4576917.git
-cat 4576917/vimrc | sed 's/consolas:h16/Inconsolata\\ 14/' > ~/.vimrc
-rm -rf 4576917
-mkdir ~/.vim
-pushd ~/.vim
-mkdir colors
-pushd colors
-wget http://files.werx.dk/wombat.vim
-dos2unix wombat.vim
-popd
-wget "http://www.vim.org/scripts/download_script.php\?src_id\=17123" -O nerdtree.zip
-unzip nerdtree.zip
-rm nerdtree.zip
-wget "http://downloads.sourceforge.net/project/vim-latex/snapshots/vim-latex-1.8.23-20130116.788-git2ef9956.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fvim-latex%2Ffiles%2Fsnapshots%2F&ts=1384542687&use_mirror=softlayer-dal" -O latex.tar.gz
-tar -xzvf latex.tar.gz
-mv vim-latex*/* .
-rm -r vim-latex*
-rm latex.tar.gz
+ln -s .dotfiles/.zshrc .zshrc
+# .vimrc
+ln -s .dotfiles/.vim .vim
+ln -s .dotfiles/.vimrc .vimrc
+cat ~/.vimrc | sed 's/consolas:h16/Inconsolata\\ 14/' > ~/.vimrc # patch for debian GUI
+# .tmux.conf
+ln -s .dotfiles/.tmux.conf .tmux.conf
 popd
 # git configuration
 git config --global user.name "Yan Wang"
