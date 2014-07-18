abc733170d0a93ecaed79af1872eaf9381c6d763
Yan Wang <grapeot@gmail.com>
2014-07-17 17:30:18 -0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 
 # Configure development environment
-sudo apt-get install -y -q vim zsh git wget dos2unix python python-setuptools sshfs
+sudo apt-get install -y -q vim zsh git wget dos2unix python python-setuptools sshfs parallel
 # shell environment...
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 pushd ~
