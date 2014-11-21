13b820a8c1453c1f5de432e352deba2651814f0a
Yan Wang <grapeot@gmail.com>
2014-11-21 12:35:38 -0500

    revise the fuse installation script. fix #10.

--- install_fuse.sh
+++ install_fuse.sh
@@ -1,7 +1,7 @@
-if [-a ~/.passwd-s3fs ]; then
+if [ ! -e ~/.passwd-s3fs ]; then
     # first run
-    sudo apt-get remove fuse
-    sudo apt-get install build-essential libcurl4-openssl-dev libxml2-dev mime-support
+    sudo apt-get -y -q remove fuse
+    sudo apt-get -y -q install build-essential libcurl4-openssl-dev libxml2-dev mime-support
 
     wget http://downloads.sourceforge.net/project/fuse/fuse-2.X/2.9.3/fuse-2.9.3.tar.gz
     tar xzf fuse-2.9.3.tar.gz
@@ -17,7 +17,7 @@ if [-a ~/.passwd-s3fs ]; then
     tar xzf s3fs-1.74.tar.gz
     pushd s3fs-1.74
     ./configure --prefix=/usr/local
-    make && sudo make install
+    make -j && sudo make install
 
     echo AWS_ACCESS_KEY_ID:AWS_SECRET_ACCESS_KEY > ~/.passwd-s3fs
     chmod 600 ~/.passwd-s3fs
