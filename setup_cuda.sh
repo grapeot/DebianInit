d450921ff940a4ed805e09ae1a19bc4f362eef30
Yan Wang <grapeot@gmail.com>
2015-02-17 20:54:18 +0000

    Fix a bug.

--- setup_cuda.sh
+++ setup_cuda.sh
@@ -5,7 +5,8 @@ sudo dpkg -i cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo apt-get update
 sudo apt-get -q -y install cuda python-numpy python-scipy python-nose libopenblas-dev
 sudo pip install theano
-echo "export CUDA_ROOT=/usr/local/cuda/bin" >> ~/.zshrc
-echo "export PATH=$PATH:$CUDA_ROOT" >> ~/.zshrc
+echo 'export CUDA_ROOT=/usr/local/cuda/bin' >> ~/.zshrc
+echo 'export PATH=$PATH:$CUDA_ROOT' >> ~/.zshrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.zshrc
 
 echo "Need to reboot for the driver to take effect."
