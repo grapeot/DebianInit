86cf94a9ccf72b2ab3cee724223d0ae46442eeee
Yan Wang <grapeot@gmail.com>
2015-02-17 16:57:39 -0500

    try to make it work

--- setup_cuda.sh
+++ setup_cuda.sh
@@ -3,7 +3,9 @@
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo apt-get update
-sudo apt-get -q -y install cuda python-numpy python-scipy python-nose libopenblas-dev
+sudo apt-get -q -y install python-numpy python-scipy python-nose libopenblas-dev linux-image-generic
+sudo apt-get -q -y dist-upgrade
+sudo apt-get -q -y install cuda
 sudo pip install theano
 echo 'export CUDA_ROOT=/usr/local/cuda/bin' >> ~/.zshrc
 echo 'export PATH=$PATH:$CUDA_ROOT' >> ~/.zshrc
