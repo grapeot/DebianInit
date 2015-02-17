a6e32a3847d259f65aee085fdf9bc4dd51c44b2e
Yan Wang <grapeot@gmail.com>
2015-02-17 20:40:10 +0000

    Add some extra configruation for cuda.

--- setup_cuda.sh
+++ setup_cuda.sh
@@ -3,6 +3,9 @@
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo apt-get update
-sudo apt-get -q -y install cuda
+sudo apt-get -q -y install cuda python-numpy python-scipy python-nose libopenblas-dev
+sudo pip install theano
+echo "export CUDA_ROOT=/usr/local/cuda/bin" >> ~/.zshrc
+echo "export PATH=$PATH:$CUDA_ROOT" >> ~/.zshrc
 
 echo "Need to reboot for the driver to take effect."
