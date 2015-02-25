0468bc7be6e30296b6ccf1de9cbf08d75d2ceb84
Yan Wang <grapeot@gmail.com>
2015-02-24 22:40:51 -0500

    Improve the mac script.

--- osx.sh
+++ osx.sh
@@ -6,14 +6,16 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 # Install some utilitis
-brew install python wget curl tmux vim git
+brew install python wget curl tmux git
+brew install vim --with-python --with-ruby --with-perl
+brew install macvim --env-std --override-system-vim
 
 # Scientific Development Environment
 pip install numpy scipy matplotlib
 
 # oh-my-zsh and z
 curl -L http://install.ohmyz.sh | sh
-pushd~
+pushd ~
 git clone https://github.com/rupa/z
 popd
 
@@ -31,7 +33,5 @@ git config --global color.ui auto
 # Other apps
 # Skim
 wget http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.4.9/Skim-1.4.9.dmg?r=http%3A%2F%2Fskim-app.sourceforge.net%2F&ts=1404764983&use_mirror=hivelocity
-# MacVim
-wget https://github.com/b4winckler/macvim/releases/download/snapshot-73/MacVim-snapshot-73-Mavericks.tbz
 # Chrome
 wget https://dl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg
