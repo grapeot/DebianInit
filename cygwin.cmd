7e6251f1d69dc8825ff8cb992e2fafa95c810c0f
Yan Wang <grapeot@gmail.com>
2015-10-29 14:24:29 -0700

    Update cygwin.cmd

--- cygwin.cmd
+++ cygwin.cmd
@@ -1,6 +1,7 @@
-.\setup-x86.exe -P vim -P openssh -P rsync -P gcc-g++ -P make -P wget -P curl -P dos2unix -P git -P tig -P zsh -P tmux -s http://mirror.cc.columbia.edu/pub/software/cygwin/
+setup-x86_64.exe -P vim -P openssh -P rsync -P gcc-g++ -P make -P wget -P curl -P dos2unix -P git -P tig -P zsh -P tmux -s http://mirror.cc.columbia.edu/pub/software/cygwin/
 pause
 
+c:\cygwin64\bin\zsh.exe -i -c "c:/cygwin64/bin/mkpasswd -c | c:/cygwin64/bin/sed 's/bash/zsh/' | c:/cygwin64/bin/sed 's/\/home/\/cygdrive\/c\/Users/' | c:/cygwin64/bin/tee /etc/passwd"
 cmd /c "set PATH=%PATH%;c:\cygwin64\bin; && zsh -i -c 'git clone https://github.com/grapeot/DebianInit.git; cd DebianInit; ./cygwin.sh'"
 echo Don't forget to add the cygwin bin path to the system PATH.
 
