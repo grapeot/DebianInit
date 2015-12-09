56f92b7a161bbe072f8b63c155e17cbe9bb50900
Yan Wang <grapeot@gmail.com>
2015-12-09 23:33:05 +0000

    add a script to install docker.

---
+++ install_docker.sh
@@ -0,0 +1,9 @@
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo bash -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get purge lxc-docker
+sudo apt-get install -y linux-image-extra-$(uname -r)
+sudo apt-get install -y docker-engine
+
+# verify whether the installation is successful.
+sudo docker run hello-world
