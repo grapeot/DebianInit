598008c2551dfd4a5050dcf8fdc753c47430da5a
Yan Wang <wanyan@microsoft.com>
2016-10-27 16:16:35 -0700

    Add a new utility.

---
+++ bin/tsv2json.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+if len(sys.argv) != 2:
+    sys.stderr.write('Usage: tsv2json.py <tsv file>\n')
+    sys.stderr.write('Convert a tsv file to a json file with each line as a json record\n')
+    sys.exit(-1)
+
+infile = sys.argv[1]
+lines = [ x.strip().split('\t') for x in open(infile) ]
+header = lines[0]
+for line in lines[1:]:
+    obj = { x: y for x, y in zip(header, line) }
+    print json.dumps(obj)
