8888b75b6e2adedbda5157eb56a8af2ab24cb0ee
Yan Wang <wanyan@microsoft.com>
2016-10-27 13:15:40 -0700

    Add some utilities for windows.

---
+++ bin/join.sh
@@ -0,0 +1,13 @@
+if [ ! $# -eq 4 ]; then
+    echo "Usage: join.sh <col1> <col2> <fn1> <fn2>"
+    exit -1
+fi
+
+col1="$1"
+col2="$2"
+fn1="$3"
+fn2="$4"
+LC_ALL=C sort -t'	' -k $col1 $fn1 | dos2unix > sort_tmp1.tsv
+LC_ALL=C sort -t'	' -k $col2 $fn2 | dos2unix > sort_tmp2.tsv
+join -t'	' -1 $col1 -2 $col2 sort_tmp1.tsv sort_tmp2.tsv
+rm sort_tmp1.tsv sort_tmp2.tsv
