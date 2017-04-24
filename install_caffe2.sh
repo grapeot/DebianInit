6325bb673d748db507d5344dea32b1cf1122b8a5
Yan Wang <grapeot@gmail.com>
2017-04-23 18:26:47 -0700

    Add a new script to set up caffe2 environment on AWS.

---
+++ install_caffe2.sh
@@ -0,0 +1,54 @@
+# From https://caffe2.ai/docs/getting-started.html?platform=ubuntu&configuration=compile
+# Dependencies
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
+      build-essential \
+      cmake \
+      git \
+      libgoogle-glog-dev \
+      libprotobuf-dev \
+      protobuf-compiler \
+      python-dev \
+      python-pip                          
+sudo pip install numpy protobuf
+# CUDA
+sudo apt-get update && sudo apt-get install wget -y --no-install-recommends
+wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda
+# cuDNN
+CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
+wget ${CUDNN_URL}
+sudo tar -xzf cudnn-8.0-linux-x64-v5.1.tgz -C /usr/local
+rm cudnn-8.0-linux-x64-v5.1.tgz && sudo ldconfig
+# Other dependencies
+sudo apt-get install -y --no-install-recommends libgflags-dev
+sudo apt-get install -y --no-install-recommends \
+      libgtest-dev \
+      libiomp-dev \
+      libleveldb-dev \
+      liblmdb-dev \
+      libopencv-dev \
+      libopenmpi-dev \
+      libsnappy-dev \
+      openmpi-bin \
+      openmpi-doc \
+      python-pydot
+sudo pip install \
+      flask \
+      graphviz \
+      hypothesis \
+      jupyter \
+      matplotlib \
+      pydot python-nvd3 \
+      pyyaml \
+      requests \
+      scikit-image \
+      scipy \
+      setuptools \
+      tornado
+# Clone and build
+git clone --recursive https://github.com/caffe2/caffe2.git && cd caffe2
+make && cd build && sudo make install
+python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
