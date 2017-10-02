b1c646d49b1d6c09251aa752be4c816750942458
Yan Wang <grapeot@gmail.com>
2017-10-01 17:23:42 -0700

    Fix a lot of issues in the windows set up script.

--- cygwin.sh
+++ cygwin.sh
@@ -5,18 +5,13 @@ cd ~
 git clone https://github.com/rupa/z
 # .dotfiles
 git clone --recursive https://github.com/grapeot/.dotfiles
-# .zshrc
-rm .zshrc
-ln -s .dotfiles/.zshrc .zshrc
-# .vimrc
-ln -s .dotfiles/.vim .vim
-ln -s .dotfiles/.vimrc .vimrc
 # .tmux.conf
 ln -s .dotfiles/.tmux.conf .tmux.conf
 cat .tmux.conf | grep -v 'powerline' | tee .tmux.conf  # Windows cannot use powerline?
 # git configuration
 git config --global user.name "Yan Wang"
-git config --global user.email grapeot@gmail.com
+git config --global user.email grapeot@outlook.com
 git config --global color.ui auto
 # ssh configuration (won't take effect until restart)
 cat /etc/sshd_config | sed 's/Port 22/Port 30/' | tee /etc/ssh/sshd_config 
+ssh-host-config
---
+++ setup_windows.ps1
@@ -0,0 +1,39 @@
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile('https://www.cygwin.com/setup-x86_64.exe', 'setup-x86_64.exe')
+$wc.DownloadFile('https://raw.githubusercontent.com/grapeot/DebianInit/master/cygwin.sh', 'cygwin.sh')
+
+
+# We use windows version for better compatibility with VSOnline and https.
+$wc.DownloadFile('https://chocolatey.org/install.ps1', 'installChocolatey.ps1')
+.\installChocolatey.ps1
+choco install -y git
+
+# Install cygwin
+.\setup-x86_64.exe -P vim -P openssh -P rsync -P gcc-g++ -P make -P wget -P curl -P dos2unix -P tig -P zsh -P tmux -s http://mirror.cc.columbia.edu/pub/software/cygwin/ | Out-Null
+c:\cygwin64\bin\zsh.exe -i -c "c:/cygwin64/bin/mkpasswd -c | c:/cygwin64/bin/sed 's/bash/zsh/' | c:/cygwin64/bin/sed 's/\/home/\/cygdrive\/c\/Users/' | c:/cygwin64/bin/tee /etc/passwd"
+cmd /c "set PATH=%PATH%;c:\cygwin64\bin; && zsh -i -c './cygwin.sh'"
+echo "Don't forget to add the cygwin bin path to the system PATH."
+
+# Install other dev tools
+choco install -y 7zip vim python3
+rm .\installChocolatey.ps1
+
+# Fix the windows version of .dotfile
+# To make symbolic links, we need to import an API from kernel32.dll
+# https://learn-powershell.net/2013/07/16/creating-a-symbolic-link-using-powershell/
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+ 
+namespace mklink
+{
+    public class symlink
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+    }
+}
+"@
+[mklink.symlink]::CreateSymbolicLink('c:/Users/grapeot/.vim',"c:/Users/grapeot/.dotfiles/.vim",1)
+[mklink.symlink]::CreateSymbolicLink('c:/Users/grapeot/.vimrc',"c:/Users/grapeot/.dotfiles/.vimrc",0)
+[mklink.symlink]::CreateSymbolicLink('c:/Users/grapeot/.zshrc',"c:/Users/grapeot/.dotfiles/.zshrc",0)
