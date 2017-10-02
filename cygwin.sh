c223fc66a3b5f8cca7b29bd38cde27c0dcc5d001
Yan Wang <grapeot@gmail.com>
2017-10-01 18:20:49 -0700

    Fix the git problem in windows.

--- cygwin.sh
+++ cygwin.sh
@@ -1,3 +1,6 @@
+# Fix the git problem
+mv /cygdrive/c/cygwin64/bin/git.exe /cygdrive/c/cygwin64/bin/git.exe.bak
+
 # Configure development environment
 # shell environment...
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
