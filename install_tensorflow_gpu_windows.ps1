8b960fba9c693eecd10258b50376f9e47740fa6f
Yan Wang <grapeot@gmail.com>
2017-10-01 18:56:28 -0700

    Update TF installation script.

--- install_tensorflow_gpu_windows.ps1
+++ install_tensorflow_gpu_windows.ps1
@@ -1,7 +1,17 @@
-# Install CUDA
 $wc = New-Object System.Net.WebClient
+
+# Install Tensorflow
+$wc.DownloadFile('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')
+python.exe .\get-pip.py
+choco install -y vcredist140
+pip install tensorflow-gpu jupyter
+
+# Install CUDA
+echo "Installing CUDA..."
 $wc.DownloadFile('https://developer.nvidia.com/compute/cuda/8.0/Prod2/network_installers/cuda_8.0.61_win10_network-exe', 'cuda_setup.exe')
 $wc.DownloadFile('https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2_windows-exe', 'cuda_patch.exe')
+$wc.DownloadFile('http://us.download.nvidia.com/Windows/Quadro_Certified/385.08/385.08-tesla-desktop-winserver2016-international-whql.exe', 'driver.exe')
+.\driver.exe -s | Out-Null
 .\cuda_setup.exe -s | Out-Null
 .\cuda_patch.exe -s | Out-Null
 
@@ -15,7 +25,3 @@ function Unzip
 }
 Unzip "cudnn.zip" "cudnn"
 cp -r ".\cudnn\cuda\*" "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\"
-
-
-# Install Tensorflow
-pip install tensorflow-gpu
