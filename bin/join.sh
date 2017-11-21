dff740a90466932858d590c196fdbe86acce8461
Yan Wang <grapeot@gmail.com>
2017-11-21 15:42:49 -0800

    Fix a bug in join.sh.

--- bin/join.sh
+++ bin/join.sh
@@ -9,5 +9,5 @@ fn1="$3"
 fn2="$4"
 LC_ALL=C sort -t'	' -k $col1 $fn1 | dos2unix > sort_tmp1.tsv
 LC_ALL=C sort -t'	' -k $col2 $fn2 | dos2unix > sort_tmp2.tsv
-join -t'	' -1 $col1 -2 $col2 sort_tmp1.tsv sort_tmp2.tsv
+LC_ALL=C join -t'	' -1 $col1 -2 $col2 sort_tmp1.tsv sort_tmp2.tsv
 rm sort_tmp1.tsv sort_tmp2.tsv
