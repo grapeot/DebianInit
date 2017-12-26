50444012ca97cfe110c280079d279e15aee12591
Yan Wang <grapeot@gmail.com>
2017-12-25 18:48:20 -0800

    Update the cudnn installation.

--- install_tensorflow_ubuntu.sh
+++ install_tensorflow_ubuntu.sh
@@ -2,7 +2,8 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda-8-0
-wget "https://www.dropbox.com/s/brc0h339ineam7n/libcudnn6_6.0.21-1%2Bcuda8.0_amd64.deb?dl=1" -O cudnn.deb
+wget "https://www.dropbox.com/s/op9hg576l36li8u/cudnn_ubuntu_8.0.deb?dl=1"
+mv "cudnn_ubuntu_8.0.deb?dl=1" cudnn.deb
 sudo dpkg -i cudnn.deb
 wget http://us.download.nvidia.com/tesla/384.66/nvidia-diag-driver-local-repo-ubuntu1604-384.66_1.0-1_amd64.deb
 sudo dpkg -i nvidia-diag-driver-local-repo-ubuntu1604-384.66_1.0-1_amd64.deb
