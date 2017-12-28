ff1a335107a88396b1374c64a94ff687a42bbf19
Yan Wang <grapeot@gmail.com>
2017-12-28 15:13:34 -0800

    Some minor tweaks

--- install_tensorflow_ubuntu.sh
+++ install_tensorflow_ubuntu.sh
@@ -1,15 +1,15 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get update
-sudo apt-get install cuda-8-0
+sudo apt-get install -y cuda-8-0
 wget "https://www.dropbox.com/s/op9hg576l36li8u/cudnn_ubuntu_8.0.deb?dl=1"
 mv "cudnn_ubuntu_8.0.deb?dl=1" cudnn.deb
 sudo dpkg -i cudnn.deb
 wget http://us.download.nvidia.com/tesla/384.66/nvidia-diag-driver-local-repo-ubuntu1604-384.66_1.0-1_amd64.deb
 sudo dpkg -i nvidia-diag-driver-local-repo-ubuntu1604-384.66_1.0-1_amd64.deb
-sudo apt-get install libcupti-dev
+sudo apt-get install -y libcupti-dev python3-dev python3-pip
 pip3 install tensorflow-gpu jupyter
-export CUDA_HOME='/usr/local/cuda-8.0'
-export LD_LIBRARY_PATH="/usr/local/cuda-8.0/lib:$LD_LIBRARY_PATH"
-export PATH="/usr/local/cuda-8.0/bin:$PATH"
-echo "Don't forget to update the .zshrc."
+echo export CUDA_HOME='/usr/local/cuda-8.0' >> ~/.zshrc
+echo export LD_LIBRARY_PATH='/usr/local/cuda-8.0/lib:$LD_LIBRARY_PATH' >> ~/.zshrc
+echo export PATH='/usr/local/cuda-8.0/bin:$PATH' >> ~/.zshrc
+source ~/.zshrc
